7a6a868a8cd213ee928e284cfb8558adbbf904da
Greg Doud <gpdoud@gmail.com>
2023-02-27 16:19:03 -0500

    Add project files.

---
+++ ef-code-first-tutorial-c39/Models/Orderline.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ef_code_first_tutorial_c39.Models;
+
+public class Orderline {
+
+    public int Id { get; set; }
+    public int Quantity { get; set; } = 1;
+
+    public int OrderId { get; set; }
+    public virtual Order Order { get; set; } = null!;
+
+    public int ItemId { get; set; }
+    public virtual Item Item { get; set; } = null!;
+
+    public Orderline() { }
+}
---
+++ ef-code-first-tutorial-c39/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using ef_code_first_tutorial_c39;
+
+using Microsoft.EntityFrameworkCore;
+
+var _context = new SalesDbContext();
+
+var order = _context.Orders
+                        .Include(x => x.Orderlines)
+                            .ThenInclude(x => x.Item)
+                        .Include(x => x.Customer)
+                        .Single(x => x.Id == 2);
+
+Console.WriteLine($"ORDER: Description: {order.Description}");
+
+foreach(var ol in order.Orderlines) {
+    Console.WriteLine($"ORDERLINE: Product: {ol.Item.Name}, Quantity: {ol.Quantity}, " +
+                        $" Price: {ol.Item.Price:C}, Line total: {ol.Quantity * ol.Item.Price:C}");
+}
+
+var orderTotal = order.Orderlines.Sum(ol => ol.Item.Price * ol.Quantity);
+
+Console.WriteLine($"Total: {orderTotal:C}");
+
+
+//_context.Customers.ToList().ForEach(c => Console.WriteLine(c.Name));
